d8c88d7d5004b0e058b928e4c855d94dc3406631
Hamish Buckmaster <HamishBuckmaster@users.noreply.github.com>
2021-07-21 17:29:25 +1000

    feat(windows): Add windows support
    Add windows support using the winget package manager.

---
+++ windows/install.bat
@@ -0,0 +1,18 @@
+:: install winget from - https://github.com/microsoft/winget-cli/releases/download/v1.0.11692
+
+winget install steam
+winget install discord
+winget install vscode
+winget install googlechrome
+winget install brave
+winget install "EA Desktop"
+winget install "GOG Galaxy"
+winget install "Epic Games Launcher"
+winget install "Battle.Net"
+winget install "SteelSeries GG"
+winget install buttercup
+winget install "CORSAIR iCUE 4 Software"
+winget install spotify
+
+:: Astro Command Center - Not available via the package manager
+:: teamspeak 5 (still in beta)
